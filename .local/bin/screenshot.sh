8115ca3e6e1c5f4d09019a798d6d75aad3413825
eug-vs <eug-vs@keemail.me>
2020-12-08 01:44:48 +0300

    refactor: move screenshot to separate script

---
+++ .local/bin/screenshot.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Screenshot via ImageMagick
+# Passes all args down to import command
+
+SCREENSHOTS_DIR=$HOME/Pictures/Screenshots
+FILENAME=$SCREENSHOTS_DIR/$(date +%F_%H-%M-%S).png
+
+echo "$@"
+
+mkdir -p $SCREENSHOTS_DIR
+import $@ $FILENAME && xclip -selection clipboard -target image/png -i < $FILENAME
---
+++ .local/bin/ttl.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Fix default TTL and disable ipv6 making changes permanent
+# TODO: replace wlo1 with automatically detected device name
+
+sudo sysctl \
+  net.ipv4.ip_default_ttl=65 \
+  net.ipv6.conf.all.disable_ipv6=1\
+  net.ipv6.conf.default.disable_ipv6=1 \
+  net.ipv6.conf.wlo1.disable_ipv6=1 \
+  | sudo tee /etc/sysctl.d/fix-ttl.conf -a
+
