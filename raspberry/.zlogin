90b9522b15a692532d9b3f28acc417670144e3fa
eug-vs <eugene@eug-vs.xyz>
2021-09-21 10:56:14 +0300

    feat(rpi): separate display and master sessions

--- raspberry/.zlogin
+++ raspberry/.zlogin
@@ -1,1 +1,8 @@
-[ -z "$TMUX" ] && tmux attach -t default || tmuxinator start default
+if [ -n "$SSH_CONNECTION" ]; then
+  # If we are logged in via SSH, automatically attach to master session
+  [ -z "$TMUX" ] && (tmux attach -t master || tmux new -s master)
+else
+  # If we are logged in directly (not via SSH)
+  # create the session which will be displayed on TV
+  tmuxinator start display
+fi
--- raspberry/temp.sh
+++ raspberry/temp.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Show CPU temperature in a cool way. Requires superuser permissions.
 
-watch -n 2 --no-title "/opt/vc/bin/vcgencmd measure_temp | sed 's/temp=//' | figlet -W | /usr/games/cowsay -f ghostbusters -n | sed 's/^/                                                        /'"
+watch -n 2 --no-title "/opt/vc/bin/vcgencmd measure_temp | sed 's/temp=//' | figlet -W | /usr/bin/cowsay -f ghostbusters -n | sed 's/^/                                                        /'"
