bbe0e7af9ff0381b1475bb414a6153f0bb947134
eug-vs <eugene@eug-vs.xyz>
2021-09-21 10:57:34 +0300

    feat(scripts): use wlan0 instead of wlo1

--- scripts/.local/bin/ttl.sh
+++ scripts/.local/bin/ttl.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # Fix default TTL and disable ipv6 making changes permanent
-# TODO: replace wlo1 with automatically detected device name
+# TODO: replace wlan0 with automatically detected device name
 
 sudo sysctl \
   net.ipv4.ip_default_ttl=65 \
   net.ipv6.conf.all.disable_ipv6=1\
   net.ipv6.conf.default.disable_ipv6=1 \
-  net.ipv6.conf.wlo1.disable_ipv6=1 \
+  net.ipv6.conf.wlan0.disable_ipv6=1 \
   | sudo tee /etc/sysctl.d/fix-ttl.conf -a
 
