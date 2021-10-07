befb47b82d6d644f59c22e401f4207fea75606a9
eug-vs <eugene@eug-vs.xyz>
2021-10-07 14:28:28 +0300

    chore(nvim): upgrade nvim-tree to the new config

--- nvim/.config/nvim/lua/filetree.lua
+++ nvim/.config/nvim/lua/filetree.lua
@@ -1,13 +1,20 @@
 local tree_cb = require'nvim-tree.config'.nvim_tree_callback
 
-vim.api.nvim_set_keymap('n', '<F3>', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
-
-vim.g.nvim_tree_auto_close = 1
-vim.g.nvim_tree_group_empty = 1
-
-vim.g.nvim_tree_bindings = {
-  { key = "l", cb = tree_cb("edit") },
-  { key = "h", cb = tree_cb("parent_node") and tree_cb("close_node") },
-  { key = "cw", cb = tree_cb("rename") },
-  { key = "o", cb = tree_cb("create") },
+require'nvim-tree'.setup {
+  auto_close = true,
+  open_on_tab = true,
+  open_on_setup = true,
+  hijack_cursor = true,
+  view = {
+    mappings = {
+      list = {
+        { key = "l", cb = tree_cb("edit") },
+        { key = "h", cb = tree_cb("parent_node") and tree_cb("close_node") },
+        { key = "cw", cb = tree_cb("rename") },
+        { key = "o", cb = tree_cb("create") },
+      }
+    }
+  }
 }
+
+vim.api.nvim_set_keymap('n', '<F3>', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
