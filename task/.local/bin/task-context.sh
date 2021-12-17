8110ce284ef1389b70948da71002e3124e84a2bf
eug-vs <eugene@eug-vs.xyz>
2021-12-17 10:39:58 +0300

    feat(task): refactor contexts

--- task/.local/bin/task-context.sh
+++ task/.local/bin/task-context.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # TODO: list contexts using taskwarrior
 
-echo "none default dev work reading writing" | sed 's/ /\n/g' | dmenu -l 10 | xargs task context
+echo "none default dev codex reading writing" | sed 's/ /\n/g' | dmenu -l 10 | xargs task context
